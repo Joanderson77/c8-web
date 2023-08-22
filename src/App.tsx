387dcf148626314091bd2fc8d263503ca4b324b1
Joanderson <mateusjoanderson@gmail.com>
2023-08-22 12:38:43 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
- 
+import { BrowserRouter } from "react-router-dom"; 
+import { AppRoutes } from "./routes/indext";
 export const App = () => {
   return (
-    <div className="App">
-      teste
-    </div>
+   <BrowserRouter>
+    <AppRoutes/>
+   </BrowserRouter>
   );
 }
 
