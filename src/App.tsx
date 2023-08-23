40edbd5d81fc7c11ca602357c90a67dd9b20179c
Joanderson <mateusjoanderson@gmail.com>
2023-08-23 12:26:37 -0300

    feat: Responsividade drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/indext";
 import { MenuLateral } from "./shared/components/indext";
+import { AppDrawerProvider } from "./shared/contexts/DrawerContext";
 import { AppThemeProvider } from './shared/contexts/ThemeContext';
 export const App = () => {
   return (
     <AppThemeProvider>
+    <AppDrawerProvider> 
     <BrowserRouter>
 
     <MenuLateral>
       <AppRoutes />
     </MenuLateral>
-    
+
     </BrowserRouter>
+    </AppDrawerProvider>
     </AppThemeProvider>
   );
 }
